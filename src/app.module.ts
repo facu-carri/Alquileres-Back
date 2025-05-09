6c62a47ef2a82152b531b5143161987145292a91
Facu <facuc4rr@gmail.com>
2025-05-08 21:33:02 -0300

    Add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskController } from './task/task.controller';
+import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, TaskController],
-  providers: [AppService],
+  imports: [
+    DatabaseModule,
+    UsersModule
+  ],
+  controllers: [
+    AppController,
+    TaskController
+  ],
+  providers: [
+    AppService
+  ],
 })
 export class AppModule {}
