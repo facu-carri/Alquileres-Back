02d31ad08c6a439b146740cc1b2da363780dc7ce
Facu <facuc4rr@gmail.com>
2025-05-12 12:33:16 -0300

    Add auth code impl

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 import { JwtModuleConfigured } from './jwt/JwtModule';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthCode } from './dto/authCode.entity';
 
 @Module({
-  imports: [UserModule, JwtModuleConfigured],
+  imports: [UserModule, JwtModuleConfigured, TypeOrmModule.forFeature([AuthCode])],
   controllers: [AuthController],
   providers: [AuthService],
 })
---
+++ src/auth/dto/authCode.dto.ts
@@ -0,0 +1,14 @@
+import { IsString, IsEmail, IsNotEmpty, MinLength, IsPhoneNumber } from 'class-validator';
+import { validationMessage, ValidatorTypes } from 'src/utils/ValidatorMessages';
+
+export class AuthCodeDto {
+
+    @IsString(validationMessage(ValidatorTypes.IsString))
+    @IsNotEmpty(validationMessage(ValidatorTypes.isNotEmpty))
+    @IsEmail({}, validationMessage(ValidatorTypes.IsEmail))
+    readonly email: string;
+
+    @IsString(validationMessage(ValidatorTypes.IsString))
+    @MinLength(4, validationMessage(ValidatorTypes.MinLength))
+    readonly code: string;
+}
