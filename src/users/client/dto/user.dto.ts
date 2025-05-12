c6f89d44c56dcce84575e504dc225b7d6a82ad03
Facu <facuc4rr@gmail.com>
2025-05-12 07:41:07 -0300

    Add nacimiento and telefono user fields

--- src/users/client/dto/user.dto.ts
+++ src/users/client/dto/user.dto.ts
@@ -1,21 +1,29 @@
-import { IsString, IsEmail, IsNotEmpty, MinLength, Validator } from 'class-validator';
+import { IsString, IsEmail, IsNotEmpty, MinLength, IsPhoneNumber } from 'class-validator';
 import { validationMessage, ValidatorTypes } from 'src/utils/ValidatorMessages';
 
 export class UserDto {
-  @IsString(validationMessage(ValidatorTypes.IsString))
-  @IsNotEmpty(validationMessage(ValidatorTypes.isNotEmpty))
-  readonly nombre: string;
+    @IsString(validationMessage(ValidatorTypes.IsString))
+    @IsNotEmpty(validationMessage(ValidatorTypes.isNotEmpty))
+    readonly nombre: string;
 
-  @IsString(validationMessage(ValidatorTypes.IsString))
-  @IsNotEmpty(validationMessage(ValidatorTypes.isNotEmpty))
-  readonly apellido: string;
+    @IsString(validationMessage(ValidatorTypes.IsString))
+    @IsNotEmpty(validationMessage(ValidatorTypes.isNotEmpty))
+    readonly apellido: string;
 
-  @IsString(validationMessage(ValidatorTypes.IsString))
-  @IsNotEmpty(validationMessage(ValidatorTypes.isNotEmpty))
-  @IsEmail({}, validationMessage(ValidatorTypes.IsEmail))
-  readonly email: string;
+    @IsString(validationMessage(ValidatorTypes.IsString))
+    @MinLength(8, validationMessage(ValidatorTypes.MinLength))
+    readonly password: string;
 
-  @IsString()
-  @MinLength(8, validationMessage(ValidatorTypes.MinLength))
-  readonly password: string;
+    readonly nacimiento: string;
+
+    // Datos de contacto
+
+    @IsString(validationMessage(ValidatorTypes.IsString))
+    @IsNotEmpty(validationMessage(ValidatorTypes.isNotEmpty))
+    @IsEmail({}, validationMessage(ValidatorTypes.IsEmail))
+    readonly email: string;
+
+    @IsString(validationMessage(ValidatorTypes.IsString))
+    @IsPhoneNumber('AR', validationMessage(ValidatorTypes.IsEmail))
+    readonly telefono: string
 }
--- src/utils/ValidatorMessages.ts
+++ src/utils/ValidatorMessages.ts
@@ -1,27 +1,33 @@
-import { ValidationArguments } from "class-validator"
+import { ValidationArguments, ValidationOptions } from "class-validator"
 
 export enum ValidatorTypes {
     "IsEmail",
     "isNotEmpty",
     "IsString",
-    "MinLength"
+    "MinLength",
+    "IsPhoneNumber"
 }
 
 const messages = {
     [ValidatorTypes.IsEmail]: "Mail invalido",
     [ValidatorTypes.isNotEmpty]: 'El nombre de usuario no debe ser nulo',
     [ValidatorTypes.IsString]: "El nombre de usuario debe ser un string",
-    [ValidatorTypes.MinLength]: "La contrasenia debe ser mayor o igual a ${0} caracteres"
+    [ValidatorTypes.MinLength]: "La contrasenia debe ser mayor o igual a ${0} caracteres",
+    [ValidatorTypes.IsPhoneNumber]: "El numero telefono es invalido"
 }
 
 function processArguments(type: ValidatorTypes, validationArguments: ValidationArguments): string {
     const { constraints } = validationArguments
     let ret = messages[type]
-    constraints.forEach((value, index) => ret = String(ret).replace("${" + index + "}", value))
+
+    if (ret.includes('${0}') && constraints.length > 0) {
+        constraints.forEach((value, index) => ret = String(ret).replace("${" + index + "}", value))
+    }
+    
     return ret
 }
 
-export function validationMessage(type: ValidatorTypes){
+export function validationMessage(type: ValidatorTypes): ValidationOptions {
     return {
         message: processArguments.bind(null, type)
     }
