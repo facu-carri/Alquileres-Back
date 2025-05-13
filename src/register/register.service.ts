31c35f42b345937f2fa38ccae961edd5d4db6c4f
Facu <facuc4rr@gmail.com>
2025-05-13 17:01:21 -0300

    Send 200 status when user register

--- src/register/register.service.ts
+++ src/register/register.service.ts
@@ -1,18 +1,20 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { UserDto } from 'src/user/dto/user.dto';
 import { UserRole } from 'src/user/user.entity';
+import { response } from 'express';
 
 @Injectable()
 export class RegisterService {
     
-    constructor(private readonly UserService: UserService){}
+    constructor(private readonly userService: UserService){}
     
-    async register(clientData: UserDto, rol: UserRole) {
-        const client = await this.UserService.findOneByEmail(clientData.email)
+    async register(usertData: UserDto, rol: UserRole) {
+        const client = await this.userService.findOneByEmail(usertData.email)
         if (client) {
             throw new BadRequestException('El mail ya se encuentra registrado')
         }
-        return await this.UserService.create(clientData, rol)
+        await this.userService.create(usertData, rol)
+        return response.status(200)
     }
 }
