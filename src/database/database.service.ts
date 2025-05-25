e8818c9ae1e1b7a3ca9f5a924551e07a463a6bf5
matilozano96 <74582367+matilozano96@users.noreply.github.com>
2025-05-25 15:19:11 -0300

    Initializer Reservas
    Genera 10 reservas aleatorias válidas para testear.
    Usa usuarios clientes y máquinas existentes.

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -3,15 +3,28 @@ import { InitializeUsers } from './initializers/users';
 import { InitializeMaquinarias } from './initializers/maquinarias';
 import { RegisterService } from 'src/register/register.service';
 import { MaquinariaService } from 'src/maquinaria/maquinaria.service';
+import { InitializeReservas } from './initializers/reservas';
+import { ReservaService } from 'src/reserva/reserva.service';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class DatabaseService {
 
     constructor(
         private readonly registerService: RegisterService,
-        private readonly maquinariaService: MaquinariaService
+        private readonly maquinariaService: MaquinariaService,
+        private readonly reservaService: ReservaService,
+        private readonly userService: UserService,
     ) {
-        new InitializeUsers(this.registerService)
-        new InitializeMaquinarias(this.maquinariaService)
+        this.initDatabase();
+    }
+
+    async initDatabase() {
+        const initializeUsers = new InitializeUsers(this.registerService);
+        await initializeUsers.init();
+        const initializeMaquinarias = new InitializeMaquinarias(this.maquinariaService);
+        await initializeMaquinarias.init();
+        const initializeReservas = new InitializeReservas(this.reservaService, this.maquinariaService, this.userService);
+        await initializeReservas.init();
     }
 }
