6b0343bfb3afb83fd63e102388b5ea3d76fd0665
matilozano96 <74582367+matilozano96@users.noreply.github.com>
2025-06-02 19:49:06 -0300

    Fix User Maq Module

--- src/maquinaria/maquinaria.module.ts
+++ src/maquinaria/maquinaria.module.ts
@@ -6,9 +6,10 @@ import { MaquinariaService } from './maquinaria.service';
 import { MaquinariaController } from './maquinaria.controller';
 import { FilesModuleByRoute } from 'src/files/files.module';
 import { ReservaService } from 'src/reserva/reserva.service';
+import { User } from 'src/user/user.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Maquinaria, Reserva]), FilesModuleByRoute('maquinaria')],
+    imports: [TypeOrmModule.forFeature([Maquinaria, Reserva, User]), FilesModuleByRoute('maquinaria')],
     controllers: [MaquinariaController],
     providers: [MaquinariaService, ReservaService],
     exports: [MaquinariaService]
