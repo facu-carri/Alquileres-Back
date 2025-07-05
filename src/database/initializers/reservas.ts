87ea00c5874174d8ac913485cdba8e7364d12e1d
matilozano96 <74582367+matilozano96@users.noreply.github.com>
2025-07-05 18:54:51 -0300

    Cambios a ejemplos
    Reordenar hardcodeados
    Comentar Random por ahora

--- src/database/initializers/reservas.ts
+++ src/database/initializers/reservas.ts
@@ -24,7 +24,7 @@ export class InitializeReservas {
         if (reservas.length > 0) return;
 
         this.genHard();
-        this.genRandom();
+        // this.genRandom();
     }
 
     async genHard() {
@@ -77,17 +77,45 @@ export class InitializeReservas {
             await this.reservaService.confirmarReserva(res.id);
             const alquiler = await this.alquilerService.findOneByCode(res.codigo_reserva);
             this.alquilerService.updateFechaInicio(alquiler.id, reserva.fecha_inicio);
+
+            // 10% con comentario
+            if (Math.random() < 0.1) {
+                await this.alquilerService.confirm(alquiler.id, 'Revisar cubiertas');
+            }
+            else await this.alquilerService.confirm(alquiler.id);
+
+            // 50% con puntuacion
             if (c % 2 === 0) {
-                if (Math.random() < 0.1) {
-                    await this.alquilerService.confirm(alquiler.id, 'Revisar cubiertas');
-                }
-                else await this.alquilerService.confirm(alquiler.id);
                 let dto = new ReseñaDto(5, 'Excelente');
                 let res = await this.alquilerService.reseñar(alquiler.id, dto, user.id);
                 this.alquilerService.updateFechaReseña(res.id, alquiler.fecha_fin);
             }
             c++;
         }
+        // test reserva vencida
+        const reserva3 = new CreateReservaDto();
+
+        reserva3.id_maquinaria = maquinaria[0].id;
+        reserva3.email = user.email;
+
+        reserva3.fecha_inicio = new Date(Date.now() - this.DAY_CONSTANT * 10);
+        reserva3.fecha_fin = new Date(reserva3.fecha_inicio.getTime() + 86400000);
+
+        await this.reservaService.create(reserva3);
+
+        // test alquiler retrasado
+        const reserva4 = new CreateReservaDto();
+
+        reserva4.id_maquinaria = maquinaria[0].id;
+        reserva4.email = user.email;
+
+        reserva4.fecha_inicio = new Date(Date.now() - this.DAY_CONSTANT * 5);
+        reserva4.fecha_fin = new Date(reserva4.fecha_inicio.getTime() + 86400000);
+        
+        let res4 = await this.reservaService.create(reserva4);
+        await this.reservaService.confirmarReserva(res4.id);
+        const alquiler4 = await this.alquilerService.findOneByCode(res4.codigo_reserva);
+        this.alquilerService.updateFechaInicio(alquiler4.id, reserva4.fecha_inicio);
     }
 
     async genRandom() {
@@ -144,30 +172,5 @@ export class InitializeReservas {
             let reseña = await this.alquilerService.reseñar(alquiler.id, dto, randomUser.id);
             this.alquilerService.updateFechaReseña(reseña.id, alquiler.fecha_fin);
         }
-
-        // test reserva vencida
-        const reserva = new CreateReservaDto();
-
-        reserva.id_maquinaria = maquinaria[0].id;
-        reserva.email = users[0].email;
-
-        reserva.fecha_inicio = new Date(Date.now() - this.DAY_CONSTANT * 10);
-        reserva.fecha_fin = new Date(reserva.fecha_inicio.getTime() + 86400000);
-
-        await this.reservaService.create(reserva);
-
-        // test alquiler retrasado
-        const reserva2 = new CreateReservaDto();
-
-        reserva2.id_maquinaria = maquinaria[0].id;
-        reserva2.email = users[0].email;
-
-        reserva2.fecha_inicio = new Date(Date.now() - this.DAY_CONSTANT * 5);
-        reserva2.fecha_fin = new Date(reserva2.fecha_inicio.getTime() + 86400000);
-        
-        let res2 = await this.reservaService.create(reserva2);
-        await this.reservaService.confirmarReserva(res2.id);
-        const alquiler2 = await this.alquilerService.findOneByCode(res2.codigo_reserva);
-        this.alquilerService.updateFechaInicio(alquiler2.id, reserva2.fecha_inicio);
     }
 }
